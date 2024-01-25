eba0d5cf06d0330d5d7a5840e6703ac350c22934
IDO <susususu07@naver.com>
2024-01-25 23:02:42 +0900

    Add files via upload

---
+++ randint_Test.py
@@ -0,0 +1,8 @@
+import random
+
+# 두 수 사이의 어떤 랜덤한 정수를 리턴하는 함수
+print(random.randint(1, 20))
+print(random.randint(1, 20))
+print(random.randint(1, 20))
+print(random.randint(1, 20))
+print(random.randint(1, 20))
---
+++ uniform_Test.py
@@ -0,0 +1,8 @@
+import random
+
+# 두 수 사이의 랜덤한 소수를 리턴하는 함수
+print(random.uniform(0, 1))
+print(random.uniform(0, 1))
+print(random.uniform(0, 1))
+print(random.uniform(0, 1))
+print(random.uniform(0, 1))
