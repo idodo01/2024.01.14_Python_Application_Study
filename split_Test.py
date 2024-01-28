940d6eba8e043bfe9b020a90b7e3b4f1e3be3942
IDO <susususu07@naver.com>
2024-01-28 19:01:13 +0900

    Update split_Test.py

--- split_Test.py
+++ split_Test.py
@@ -1,15 +1,13 @@
-# split
 my_string = "1, 2, 3, 4, 5"
 
-# ,를 기준으로 나누기
+# split
+# 1. ","를 기준으로 나누기
 print(my_string.split(","))
 
-# 앞 뒤 공백을 제거하는 법
-# 1. strip()
-# 2. ,(공백)를 기준으로 나누기
+# 2. ", "를 기준으로 나누기
 print(my_string.split(", "))
 
-# split는 list로 값을 담는다
+# 3. split는 list로 값을 나누어 담는다
 my_string_data = my_string.split(", ")
 
 # 각각 출력하려면 list[index]
