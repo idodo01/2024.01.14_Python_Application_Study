2920377fb9e21ae9134be8ae07feeda7aae102cf
IDO <susususu07@naver.com>
2024-02-03 14:24:19 +0900

    Add files via upload
    프로젝트: 로또 시뮬레이션
    04. 로또 시뮬레이션: 겹치는 번호 개수

---
+++ list_matching_count_while_repeat.py
@@ -0,0 +1,17 @@
+def count_matching_numbers(numbers, winning_numbers):
+    # 여기에 코드를 작성하세요
+    
+    i, j = 0, 0
+    count_matching = 0
+    
+    while i < len(numbers)-1 :
+        while j < len(winning_numbers) :
+            if numbers[i] == winning_numbers[j] :
+                count_matching += 1
+            j += 1
+        j = 0    
+        i += 1
+    return count_matching    
+
+print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
+print(count_matching_numbers([2, 7, 11, 14, 25, 40], [14]))
